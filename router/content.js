88270e03cd6b7c3ca0bb1a9a0af3fe37d15dd6d4
Jong44 <mat_skak45@gmail.com>
2023-04-06 20:20:42 +0700

    update

--- router/content.js
+++ router/content.js
@@ -5,6 +5,6 @@ router.post('/', contentController.create);
 router.get('/', contentController.getAll);
 router.get('/:id', contentController.getById);
 router.put('/:id', contentController.update);
-router.put('/:id', contentController.update);
+router.delete('/:id', contentController.delete);
 
 module.exports = router;
